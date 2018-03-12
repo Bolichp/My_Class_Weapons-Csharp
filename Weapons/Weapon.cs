d36c86a734c17338aea8695d9609ef6cf105770f
Bolichp <dmbolik@gmail.com>
2018-03-12 17:00:38 +0300

    second commit

--- Weapons/Weapon.cs
+++ Weapons/Weapon.cs
@@ -8,6 +8,37 @@ namespace Weapons
 {
     class Weapon
     {
+        private string name;
+        private float caliber;
+        private double range;
 
+        public string Name { get; set; }
+        public float Caliber { get; set; }
+        public double Range { get; set; }
+
+        public Weapon()
+        {
+            Name = null;
+            Caliber = 0;
+            Range = 0;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите хар-ку оружия");
+            Console.WriteLine("Введите название: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите калибр: ");
+            Caliber = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите дальность: ");
+            Range = Convert.ToDouble(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine("Хар-ка оружия");
+            Console.WriteLine("Название " + Name);
+            Console.WriteLine("Калибр " + Caliber);
+            Console.WriteLine("Дальность " + Range);
+        }
     }
 }
